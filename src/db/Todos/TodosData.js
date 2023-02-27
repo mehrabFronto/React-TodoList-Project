f3253abf0a5cbab5568961699636978f6e5bdbff
MehrabD20 <mehrab20@gmail.com>
2023-02-27 16:45:47 +0330

    data base and local data added

---
+++ src/db/Todos/TodosData.js
@@ -0,0 +1,32 @@
+export const todosDate = [
+   {
+      id: 1,
+      title: "todo1",
+      createdAt: "2023-02-27T13:13:42.970Z",
+      isCompleted: false,
+   },
+   {
+      id: 2,
+      title: "todo2",
+      createdAt: "2023-02-27T13:13:43.947Z",
+      isCompleted: false,
+   },
+   {
+      id: 3,
+      title: "todo3",
+      createdAt: "2023-02-27T13:13:44.739Z",
+      isCompleted: false,
+   },
+   {
+      id: 4,
+      title: "todo4",
+      createdAt: "2023-02-27T13:13:45.749Z",
+      isCompleted: false,
+   },
+   {
+      id: 5,
+      title: "todo5",
+      createdAt: "2023-02-27T13:13:46.489Z",
+      isCompleted: false,
+   },
+];

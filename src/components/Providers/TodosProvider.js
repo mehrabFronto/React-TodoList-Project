8853c9c9c42354edcfcc906d6777a29c7f88cf61
MehrabD20 <mehrab20@gmail.com>
2023-03-01 18:35:35 +0330

    addTodo case added and local data deleted

--- src/components/Providers/TodosProvider.js
+++ src/components/Providers/TodosProvider.js
@@ -4,7 +4,29 @@ import todosData from "../../db/Todos/TodosData";
 const TodosContext = React.createContext();
 const TodosContextDispatcher = React.createContext();
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+   switch (action.type) {
+      case "addTodo": {
+         if (!action.title) {
+            alert("please enter the todo title...");
+            return state;
+         }
+
+         return [
+            ...state,
+            {
+               id: Math.floor(Math.random() * 1000),
+               title: action.title,
+               createdAt: new Date().toISOString(),
+               isCompleted: false,
+            },
+         ];
+      }
+
+      default:
+         return state;
+   }
+};
 
 const TodosProvider = ({ children }) => {
    const [todos, dispatch] = useReducer(reducer, todosData);
--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,12 +1,16 @@
 import Header from "../Header/Header";
 import TodoForm from "../TodoForm/TodoForm";
 import TodoList from "../TodoList/TodoList";
+import { useTodosActions } from "../Providers/TodosProvider";
 
 const TodoApp = () => {
+   const dispatch = useTodosActions();
    return (
       <div className="app">
          <Header />
-         <TodoForm />
+         <TodoForm
+            onSubmitTodo={(title) => dispatch({ type: "addTodo", title })}
+         />
          <TodoList />
       </div>
    );

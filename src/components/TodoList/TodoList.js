d63da23a6c21ff80f3a216a7969a383b1b84e89e
MehrabD20 <mehrab20@gmail.com>
2023-03-01 22:10:08 +0330

    editTodo case added and some components edited

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,10 +1,29 @@
 import { useTodos, useTodosActions } from "../Providers/TodosProvider";
 import Todo from "../Todo/Todo";
 import styles from "./todoList.module.css";
+import { useState } from "react";
+import TodoForm from "../TodoForm/TodoForm";
 
 const TodoList = () => {
    const todos = useTodos();
    const dispatch = useTodosActions();
+   const [edit, setEdit] = useState({
+      id: null,
+      title: "",
+      updated: "",
+      isCompleted: false,
+   });
+
+   const editTodoHandler = (todoTitle) => {
+      if (!todoTitle) return alert("please enter a todo...");
+      dispatch({ type: "editTodo", id: edit.id, todoTitle });
+      setEdit({
+         id: null,
+         title: "",
+         updated: "",
+         isCompleted: false,
+      });
+   };
 
    const renderTodos = () => {
       if (todos.length === 0)
@@ -28,6 +47,7 @@ const TodoList = () => {
                      onComplete={() =>
                         dispatch({ type: "completeTodo", id: todo.id })
                      }
+                     onEdit={() => setEdit(todo)}
                   />
                );
             })}
@@ -35,7 +55,18 @@ const TodoList = () => {
       );
    };
 
-   return <>{renderTodos()}</>;
+   return (
+      <>
+         {edit.id ? (
+            <TodoForm
+               onSubmitTodo={(todoTitle) => editTodoHandler(todoTitle)}
+               edit={edit}
+            />
+         ) : (
+            renderTodos()
+         )}
+      </>
+   );
 };
 
 export default TodoList;

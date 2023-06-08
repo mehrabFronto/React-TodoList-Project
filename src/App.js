ac86a90aa601251a6980f9169ed038ae155d093f
MehrabD20 <mehrab20@gmail.com>
2023-06-08 09:26:39 +0330

    react-toastify added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import TodosProvider from "./components/Providers/TodosProvider";
 import TodoApp from "./components/TodoApp/TodoApp";
 import "./app.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
    return (
       <div className="app">
+         <ToastContainer style={{ fontSize: "1.6rem" }} />
          <TodosProvider>
             <TodoApp />
          </TodosProvider>

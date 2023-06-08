80780fcb0dd2512a28ca6a24e0d591f1e87dec5c
MehrabD20 <mehrab20@gmail.com>
2023-06-08 09:20:15 +0330

    responsive style for mobile and ipad

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
 import styles from "./todo.module.css";
-import { BiCheckCircle } from "react-icons/bi";
-import { BiEdit } from "react-icons/bi";
-import { BiTrash } from "react-icons/bi";
+import {
+   BiCheckCircle,
+   BiDotsVerticalRounded,
+   BiTrash,
+   BiEdit,
+} from "react-icons/bi";
 
 const Todo = ({ todo, onRemove, onComplete, onEdit }) => {
+   const [isOpen, setIsOpen] = useState(false);
+   // get the window size
+   const [windowSize, setWindowSize] = useState(getWindowSize());
+
+   // get the window size on resize
+   useEffect(() => {
+      function handleWindowResize() {
+         setWindowSize(getWindowSize());
+      }
+
+      window.addEventListener("resize", handleWindowResize);
+
+      return () => {
+         window.removeEventListener("resize", handleWindowResize);
+      };
+   }, []);
+
+   function getWindowSize() {
+      const { innerWidth, innerHeight } = window;
+      return { innerWidth, innerHeight };
+   }
+
    return (
       <div
+         style={{
+            marginBottom: isOpen && windowSize.innerWidth < 768 ? "6rem" : "",
+         }}
          className={`${styles.todo} ${
-            todo.isCompleted ? styles.complete : null
+            todo.isCompleted ? styles.complete : ""
          }`}>
          {/* tood details */}
          <div className={styles.todo__detail}>
@@ -29,7 +58,7 @@ const Todo = ({ todo, onRemove, onComplete, onEdit }) => {
                   todo.isCompleted ? styles.lineThrow : null
                }`}></span>
          </div>
-         {/* todo buttons */}
+         {/* todo buttons desktop */}
          <div className={styles.todo__buttonsConatiner}>
             {/* remove butoon */}
             <button
@@ -48,10 +77,41 @@ const Todo = ({ todo, onRemove, onComplete, onEdit }) => {
             {/* complete butoon */}
             <button
                className={styles.btn}
-               s
                onClick={onComplete}>
                <BiCheckCircle />
             </button>
+         </div>
+         {/* todo buttons mobile */}
+         <div className={styles.todo__buttonsConatinerOnMobile}>
+            <button
+               className={`${styles.btn} ${styles.moreBtn}`}
+               onClick={() => setIsOpen((prevState) => !prevState)}>
+               <BiDotsVerticalRounded />
+            </button>
+            {isOpen && (
+               <div className={styles.buttonsOnMobile}>
+                  {/* remove butoon */}
+                  <button
+                     className={styles.btn}
+                     onClick={onRemove}>
+                     <BiTrash />
+                  </button>
+
+                  {/* edit butoon */}
+                  <button
+                     className={styles.btn}
+                     onClick={onEdit}>
+                     <BiEdit />
+                  </button>
+
+                  {/* complete butoon */}
+                  <button
+                     className={styles.btn}
+                     onClick={onComplete}>
+                     <BiCheckCircle />
+                  </button>
+               </div>
+            )}
          </div>
       </div>
    );
